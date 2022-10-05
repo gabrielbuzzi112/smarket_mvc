7689bfbdf8be6b90357f741cdbfebf5c36ff6d56
Gabriel Buzzi Sanches <gabrielbuzzi2@gmail.com>
2022-10-05 00:28:57 -0300

    Pagina root Design

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const fs = require('fs');
 
 router.get('^/$|/index(.html)?', (req, res) => {
     res.render('index.pug');
+});
+router.get('/logo', (req, res) => {
+    var img = fs.readFileSync(path.join(__dirname, '../public/img/logo.png'));
+    res.writeHead(200, {'Content-Type': 'image/gif' });
+    res.end(img, 'binary');
 });
 
 module.exports = router;
